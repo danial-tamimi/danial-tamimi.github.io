10459a3dfd59e6e1d9e53737602b180d58472afe
danial-tamimi <danialtamimi40@outlook.com>
2023-03-23 21:01:39 +0400

    finish nav in xs

--- js/index.js
+++ js/index.js
@@ -1,19 +1,64 @@
 // Submenu xsm sm med
 const SUBMENU = document.querySelector(".submenu");
-const MENUBUTTON = document.querySelector(".menuButton");
-let openMenu = document.querySelector(".openMenu");
 let closeMenu = document.querySelector(".closeMenu");
+const MENUBUTTON = document.querySelector(".menuButton");
+// Open Sub Menu
 MENUBUTTON.addEventListener("click", function () {
-    if (closeMenu.classList.contains("d-none")) {
-        SUBMENU.classList.add("submenuHeight100");
-        openMenu.classList.toggle("d-none");
-        closeMenu.classList.toggle("d-none");
+    SUBMENU.classList.remove("closeSubMenu");
+    SUBMENU.classList.add("openSubMenu");
+});
+
+// Close Sub Menu
+closeMenu.addEventListener("click", function () {
+    SUBMENU.classList.remove("openSubMenu");
+    SUBMENU.classList.add("closeSubMenu");
+});
+// Retreat SubMenu XS
+const RETREATMENU = document.querySelector(".motherRetreatSubMenu");
+const RETREATSUBMENU = document.querySelector(".retreatSubMenu");
+let retreatMenuChevron = document.querySelector(".retreatMenuChevron");
+RETREATMENU.addEventListener("click", function () {
+    if (THERAPYSUBMENU.classList.contains("h-100")) {
+        THERAPYSUBMENU.classList.add("h-0");
+        therapyMenuChevron.classList.add("bi-chevron-down");
+        therapyMenuChevron.classList.remove("bi-chevron-up");
+    }
+    if (RETREATSUBMENU.classList.contains("h-0")) {
+        RETREATSUBMENU.classList.remove("h-0");
+        RETREATSUBMENU.classList.add("h-100");
+        retreatMenuChevron.classList.remove("bi-chevron-down");
+        retreatMenuChevron.classList.add("bi-chevron-up");
     }
 
     else {
-        SUBMENU.classList.remove("submenuHeight100");
-        SUBMENU.classList.add("h-0");
-        openMenu.classList.toggle("d-none");
-        closeMenu.classList.toggle("d-none");
+        RETREATSUBMENU.classList.add("h-0");
+        RETREATSUBMENU.classList.remove("h-100");
+        retreatMenuChevron.classList.add("bi-chevron-down");
+        retreatMenuChevron.classList.remove("bi-chevron-up");
+    }
+});
+
+// Therapy SubMenu XS
+const THERAPYMENU = document.querySelector(".motherTherapySubMenu");
+const THERAPYSUBMENU = document.querySelector(".therapySubMenu");
+let therapyMenuChevron = document.querySelector(".therapyMenuChevron");
+THERAPYMENU.addEventListener("click", function () {
+    if (RETREATSUBMENU.classList.contains("h-100")) {
+        RETREATSUBMENU.classList.add("h-0");
+        retreatMenuChevron.classList.add("bi-chevron-down");
+        retreatMenuChevron.classList.remove("bi-chevron-up");
+    }
+    if (THERAPYSUBMENU.classList.contains("h-0")) {
+        THERAPYSUBMENU.classList.remove("h-0");
+        THERAPYSUBMENU.classList.add("h-100");
+        therapyMenuChevron.classList.remove("bi-chevron-down");
+        therapyMenuChevron.classList.add("bi-chevron-up");
+    }
+
+    else {
+        THERAPYSUBMENU.classList.add("h-0");
+        THERAPYSUBMENU.classList.remove("h-100");
+        therapyMenuChevron.classList.add("bi-chevron-down");
+        therapyMenuChevron.classList.remove("bi-chevron-up");
     }
 });
